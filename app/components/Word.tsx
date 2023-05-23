a362c34eaf95115f5de6179572497a6282b9fbae
carsonkrueger <crkruege@gmail.com>
2023-05-22 18:43:14 -0600

    working on word.tsx

--- app/components/Word.tsx
+++ app/components/Word.tsx
@@ -1,3 +1,28 @@
-const Word = () => {
-  return <></>;
+import Letter from "./Letter";
+
+interface props {
+  word: string;
+  addKeyPress: () => void;
+  removeKeyPress: () => void;
+  addErrorPress: () => void;
+}
+
+// LETTER PROPS:
+// interface props {
+//   trueLetter: string;
+//   userLetter: string;
+//   isFrontLetter: boolean;
+//   addKeyPress: () => void;
+//   removeKeyPress: () => void;
+//   addErrorPress: () => void;
+// }
+
+const Word = ({ word }: props) => {
+  return (
+    <div>
+      {word.split("").map(() => (
+        <Letter />
+      ))}
+    </div>
+  );
 };
