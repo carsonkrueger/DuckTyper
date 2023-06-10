8533aa7ae4a4180e82824cc8d77d7a6a40f4b8f4
carsonkrueger <crkruege@gmail.com>
2023-06-09 21:50:43 -0600

    working on leaderboard styling

--- app/components/RecordRow.tsx
+++ app/components/RecordRow.tsx
@@ -9,12 +9,10 @@ export default function RecordRow({ record, idx }: props) {
   const isEven = idx % 2 === 0;
   return (
     <div
-      className={`flex p-1 [&>p]:min-w-[22.5%]  text-center ${
-        isEven ? "bg-secondaryLowlight text-secondary" : "text-white"
-      }`}
+      className={`flex p-1 py-3 rounded-xl [&>p]:min-w-[21%]  text-center bg-secondaryHighlight`}
     >
-      <div className="min-w-[10%]">{idx}</div>
-      <p className="text-start pl-1">{record.username}</p>
+      <div className="min-w-[14%] text-white">{idx}</div>
+      <p className="text-start  pl-1">{record.username}</p>
       <p>{record.wpm}</p>
       <p>{record.difficulty}</p>
       <p>{record.timer}</p>
