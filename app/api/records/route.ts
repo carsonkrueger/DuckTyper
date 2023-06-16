b1c7ace73789604ed241701751099efe4a91d7a2
carsonkrueger <crkruege@gmail.com>
2023-06-16 11:38:34 -0600

    working on endpoints for getting and creating records, added some leaderboard styles

--- app/api/records/route.ts
+++ app/api/records/route.ts
@@ -3,10 +3,59 @@ import { NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { Record } from "@/types/types";
 
+async function getAllRecordsDESC() {
+  return await prisma.record.findMany({
+    orderBy: [{ wpm: "desc" }],
+  });
+}
+
+async function updateRecord(index: number, record: Record) {
+  return await prisma.record.update({
+    data: record,
+    where: {
+      id: index,
+    },
+  });
+}
+
+async function createNewRecord(record: Record) {
+  return await prisma.record.create({
+    data: record,
+  });
+}
+
+prisma.$use(async (params, next) => {
+  if (params.model === "record") {
+    switch (params.action) {
+      case "create": {
+        const DataTable: Record[] = await getAllRecordsDESC();
+        // update record
+        if (DataTable.length >= 100) {
+          const lastWpm = DataTable[DataTable.length - 1].wpm;
+          if (params.args.record.wpm > lastWpm) {
+            for (let i = 0; i < DataTable.length; i++) {
+              if (DataTable[i].wpm < params.args.record.wpm)
+                updateRecord(i, params.args.record);
+            }
+          }
+        }
+        // create new record
+        else {
+          createNewRecord(params.args.record);
+        }
+      }
+      default:
+        break;
+    }
+  }
+  const result = await next(params);
+  return result;
+});
+
 export async function GET() {
   try {
-    const data: Record[] = await prisma.record.findMany();
-    return NextResponse.json({ body: data });
+    const table: Record[] = await getAllRecordsDESC();
+    return NextResponse.json({ body: table });
   } catch (error) {
     throw error;
   }
--- app/components/LeaderBoard.tsx
+++ app/components/LeaderBoard.tsx
@@ -51,14 +51,14 @@ export default async function LeaderBoard() {
       <h1 className="min-w-full text-2xl text-center text-secondary">
         Leader Board
       </h1>
+      <div className="mx-auto flex p-1 max-w-lg w-[16rem] md:w-[26rem] lg:w-[32rem] [&>p]:min-w-[21%] [&>p]:pl-1 [&>p]:text-center">
+        <div className="min-w-[14%] text-center">Rank</div>
+        <p className="text-start">Name</p>
+        <p>WPM</p>
+        <p>Diff</p>
+        <p>Time</p>
+      </div>
       <div className="flex flex-col mx-auto overflow-y-scroll space-y-2 scrollbar rounded-lg max-w-lg w-[16rem] md:w-[26rem] lg:w-[32rem] h-[48rem] max-h-[90vh]">
-        <div className="p-1 flex [&>p]:min-w-[21%] [&>p]:pl-1 [&>p]:text-center">
-          <div className="min-w-[14%] text-center">Rank</div>
-          <p className="text-start">Name</p>
-          <p>WPM</p>
-          <p>Diff</p>
-          <p>Time</p>
-        </div>
         {data.body.map((rec: Record, idx: number) => (
           <RecordRow record={rec} idx={idx} />
         ))}
